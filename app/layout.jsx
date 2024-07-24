faf0a4ae8c0fbf4f34171ba1c9d73f713667861b
Marabunta12 <s210869@sggw.edu.pl>
2024-07-25 00:02:03 +0200

    update layout file

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,23 +1,27 @@
-import "@styles/globals.css"
+import "@styles/globals.css";
+
+import Nav from "@components/Nav";
+import Provider from "@components/Provider";
 
 export const metadata = {
-    title: "Promptopia",
-    description: "Discover & Share AI Prompts"
-}
+	title: "Promptopia",
+	description: "Discover & Share AI Prompts",
+};
 
-const RootLayout = ({children}) => {
-  return (
-    <html lang="en">
-      <body>
-        <div className="main">
-          <div className="gradient"/>
-        </div>
-        <main className="app">
-          {children}
-        </main>
-      </body>
-    </html>
-  )
-}
+const RootLayout = ({ children }) => {
+	return (
+		<html lang="en">
+			<body>
+				<div className="main">
+					<div className="gradient" />
+				</div>
+				<main className="app">
+					<Nav></Nav>
+					{children}
+				</main>
+			</body>
+		</html>
+	);
+};
 
-export default RootLayout
+export default RootLayout;
